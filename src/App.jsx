0b0b1e9ae70b5f0cc4a5c99b240696f57e888e8e
Juan José Rodriguez <zoranbow@gmail.com>
2023-04-25 06:20:49 -0500

    Gamma

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Gamma from "./components/Gamma"
+
+function App() {
+  return (
+    <>
+      <Gamma/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Gamma.jsx
@@ -0,0 +1,9 @@
+import s from './Gamma.module.css'
+
+export default function Gamma() {
+  return (
+    <div className={s.container}>
+      <div className={s.gradient}></div>
+    </div>
+  )
+}
